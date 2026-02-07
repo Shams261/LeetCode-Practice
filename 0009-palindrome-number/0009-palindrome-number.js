b4960575eaed4169d0734d9c8997c8ccc5ba9ec0
Shams Tabrez <shamsshoaib261@gmail.com>
2026-02-06 21:35:00 -0500

    Time: 10 ms (31.06%), Space: 63.5 MB (75.2%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    let xCopy = x
+    if(x<0) return false
+    let rev= 0
+    while(x>0){
+        let rem = x%10
+        rev= (10*rev) + rem
+        x =Math.floor(x/10)
+    }
+    return rev===xCopy
+    
+};
