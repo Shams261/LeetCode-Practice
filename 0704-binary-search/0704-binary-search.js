2c580c1f5f7cb5cebb60cb060192d328387f44c7
Shams Tabrez <shamsshoaib261@gmail.com>
2026-02-21 12:06:32 -0500

    Time: 0 ms (100%), Space: 58.4 MB (38.66%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -4,12 +4,18 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    for (let i =0 ;i <nums.length ;i++){
-        if(nums[i]=== target){
-            return i
+    let left =0
+    let right = nums.length-1
+    while(right>=left){
+        let middle = Math.floor((left+right)/2)
+        if(target === nums[middle]){
+            return middle
         }
-        
-        
+        else if (target < nums[middle]){
+            right = middle -1
+        }else{
+            left = middle+1
+        }
     }
     return -1
     
