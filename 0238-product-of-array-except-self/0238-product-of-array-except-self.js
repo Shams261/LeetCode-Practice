c8b72afb242321d4adc42f7ee58fe1347a9b2b14
Shams Tabrez <shamsshoaib261@gmail.com>
2026-03-28 17:42:36 -0400

    Time: 1 ms (98.91%), Space: 70.8 MB (56.14%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -4,22 +4,20 @@
  */
 var productExceptSelf = function(nums) {
     let n = nums.length
-    let left = new Array(n)
-    let right = new Array(n)
     let result = new Array(n)
 
-    left[0]=1
-    for(let i =1 ; i<n;i++){
-        left[i] = left[i-1] * nums[i-1]
+    result[0] =1
+    for(let i =1;i<n;i++){
+        result[i] = result[i-1] * nums[i-1]
     }
-    right[n-1] =1 
-    for(let i = n-2 ; i>=0 ;i--){
-        right[i] = right[i+1] * nums[i+1]
-    }
-    for(let i=0;i<n;i++){
-        result[i] = left[i] * right[i]
+    let right = 1
+    for(let i=n-1; i>=0 ;i--){
+        result[i] = result[i] * right
+        right = right * nums[i]
     }
     return result
+
+    
 };
 
 
@@ -27,25 +25,34 @@ var productExceptSelf = function(nums) {
 
 // nums [2,3,4,5]
 
-// o/p = [60,40,30,24]
+// let result = [] 
 
-// left half and the right then in the final it ewould that will the mul tipplication of left and right
+// result[0]=1
+// i<1;i<n;i++
+// {
+//     i = 1
+//     result[i] = result[i-1]* nums[i-1]
+//     result[1] = 1 * 2 = 2
+//     result[2] = 2 *3 = 6
+//     result[3] = 6 * 4 = 24
+//     result = [1,2,6,24]
+// }
+// right = 1
+// n-1;n>=0;i--{
+//     reult[i] = result[i] * right
+//     right = right * nums[i]
+// }
+// i = 3 , 
+// result[3] = 24 * 1 = 24 => [1,2,6,24]
+// right = 1 * 5 = 5
 
-// 2,3,4,5
-// left[0] = 2> = 1,
-// 3> 2
-// 4> 6
-// 5>24
-// left = 1,2,6,24
+// result [2] = 6 * 5 = 30 => [1,2,30,24]
+// right = 5 * 4 = 20
 
-// right [n-1] =1
-// n-2 : 5
-// n-3 : 20
-// n-4 : 60
+// result[1] = 2 * 20 =40 > [1,40,30,24]
+// right = 20*3 =60
 
-// right = 60,20,5,1
+// i =0 => 1 * 60 => [60,40,30,24]
+// rgiht = 60
 
-// result = left * right 
-
-// o(n), o(n))
-//  [60,40,30,24]
+// [[60,40,30,24]]
