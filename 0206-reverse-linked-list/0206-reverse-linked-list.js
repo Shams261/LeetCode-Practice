986cd881b261afc1f3cceefe47e358716bdac3c7
Shams Tabrez <shamsshoaib261@gmail.com>
2026-03-29 21:49:27 -0400

    Time: 0 ms (100%), Space: 58.3 MB (16.81%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.js
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -10,14 +10,44 @@
  * @return {ListNode}
  */
 var reverseList = function(head) {
-    let prev = null
-    let curr = head
-    while(curr){
-        let temp = curr.next
-        curr.next = prev
-        prev = curr
-        curr = temp
-    }
-    return prev
+
+    if(!head || head.next===null) return head
+
+    let newHead = reverseList(head.next)
+    head.next.next = head
+    head.next =null
+    return newHead
+   
     
 };
+
+
+//  1>2>3>null : 3>2>1>null
+
+//  !head or head.next = null return head
+
+//  recursive 1
+//  recursive 2
+//  recursive 3
+
+// R3 : head.next =null
+// return head = >3
+// this is my new head = 3
+
+// R2 : 
+// head =2
+// head.next = 3 
+
+// head.next.next = head > 3.next = 2 : 3>2
+// head.next = null > 2.next = null : 3>2>null
+
+// head.next = 2
+
+// head =1 1>2
+// head.next = 2
+// head.next.next = head => 2.next =1
+// head.next = null
+// 3>2>1>null
+// return his as newHead 
+// t 0 n
+// s 01
