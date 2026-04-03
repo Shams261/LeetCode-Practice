998ac100177a71b65829427ba3c0128839b265c9
Shams Tabrez <shamsshoaib261@gmail.com>
2026-04-03 08:07:23 -0400

    Time: 0 ms (100%), Space: 54.5 MB (86.92%) - LeetHub

--- 0078-subsets/0078-subsets.js
+++ 0078-subsets/0078-subsets.js
@@ -6,13 +6,32 @@ var subsets = function(nums) {
     let result = []
     let backtrack = (path,start) =>{
         result.push([...path])
-        for(let i=start ;i<nums.length;i++){
+        for(let i= start ; i<nums.length;i++){
             path.push(nums[i])
             backtrack(path,i+1)
             path.pop()
+
         }
     }
     backtrack([],0)
     return result
-    
 };
+
+
+// constraint = unique
+// order coule be any
+
+// nums  = [46],5, => 2^n = 8
+
+// [[]=>[4]=>[5]=>[6]=>[4,5]=>[4,6]=>[4,5,6]=>[5,6]]
+
+//            []
+    
+//      [4]         [5]         [6]
+
+// [4,5] [4,6]      [5,6]
+
+// [4,5,6]
+
+// T = O(2^n.n)
+// s = O(2^n)
