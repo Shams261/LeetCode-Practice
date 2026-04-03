8224ef4662984de0d8752057872a3b4f28f7d25d
Shams Tabrez <shamsshoaib261@gmail.com>
2026-04-02 22:01:09 -0400

    Time: 64 ms (38.35%), Space: 53 MB (87.54%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -3,8 +3,19 @@
  * @return {number}
  */
 var fib = function(n) {
-    if (n===0) return 0
+    if(n===0) return 0
     if(n===1) return 1
     return fib(n-1)+fib(n-2)
     
 };
+
+
+// f(n) = f(n-1)+f(n-2)
+// f(0) =0
+// f(1) =1
+// f(2) = 1 f(1)+f(0) : 1+0 = 1
+
+// f(5) = f(4)+f(3) =>3+2 =5
+// f(4) = f(3) + f(2) =>2+1 =3
+// f(3) =f(2)+f(1) => 2
+// f(2) = f(1) +f(0) > 1
