c6ca0d6d495c19e3d96db29abc82807c91ce73af
Shams Tabrez <shamsshoaib261@gmail.com>
2026-04-22 17:49:12 -0400

    Time: 0 ms (100%), Space: 55.6 MB (9.34%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var balancedStringSplit = function(s) {
+    let L =0
+    let R =0
+    let count =0
+    for(let i=0;i<s.length;i++){
+        if(s[i]==="L"){
+            ++L
+        }else{
+            R++
+        }
+        if(L===R){
+            count++
+            L=0
+            R=0
+
+        }
+    }
+    return count
+    
+};
