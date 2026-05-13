4fbcebb05d91e52b8b81a2f853b49b953a52ec83
Shams Tabrez <shamsshoaib261@gmail.com>
2026-05-13 06:48:24 -0400

    Time: 1 ms (34.19%), Space: 56.7 MB (77.79%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -11,30 +11,36 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
+
     if(!list1) return list2
     if(!list2) return list1
 
-
-// dummy node will start and rest the element will attach to it
     let start = new ListNode()
     let curr = start
-//comparing both list node whomever is small will attach to dummy and then dummy node will move forward
+
     while(list1 && list2){
-        if(list1.val<list2.val){
+        if(list1.val < list2.val){
             curr.next = list1
             list1 = list1.next
         }else{
-            curr.next =list2
+            curr.next = list2
             list2 = list2.next
         }
+
         curr = curr.next
     }
-    //if l1 is now comppleted then whatever the remaining node of l2 it will attach to the curr node same vice versa for l2 case
     if(!list1){
         curr.next = list2
     }
     if(!list2){
-        curr.next =list1
+        curr.next = list1
     }
-    return start.next
+    return start.next 
 };
+
+// 2>4>8>10>null
+
+// 1>3>>5>6>11>14>null
+
+// DummyHead = dummyHead.next= smaller
+// dummyHead>1>2>3>4>5>6>8>10>11>14>null => O(n) , O(1)
