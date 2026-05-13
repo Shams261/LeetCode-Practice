e6ee603e72909a266e02ca1cf4475699580d15a2
Shams Tabrez <shamsshoaib261@gmail.com>
2026-05-13 07:43:38 -0400

    Time: 53 ms (62.67%), Space: 59.2 MB (54.36%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -11,16 +11,40 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    if(head===null)return false
-    let slow = head
-    let fast = head.next
-    while(slow!=fast){
-        if(fast===null || fast.next===null){
-            return false
-        }
-        slow = slow.next
-        fast = fast.next.next
+    if (head===null) return false
+   let slow = head
+   let fast = head.next
+
+ 
+
+   while(slow!=fast){
+    if(fast === null || fast.next === null){
+        return false
     }
-    return true
+    slow = slow.next
+    fast = fast.next.next
+   }
+   return true
     
 };
+
+// 2>4>5>6>7   : true
+//         |
+//         4
+
+// 4>6>8>2>null : false : [4,6,8,2] : false
+
+// T = o(n)
+// S =O(n)
+
+// set => [2,4,5,6,7,]: true
+
+
+ // Floyd's Algorithm 
+// slow= 1 step
+// fast = 2 step
+
+// if slow == fast :true else : false
+
+2>4>5>6>7>4
+
