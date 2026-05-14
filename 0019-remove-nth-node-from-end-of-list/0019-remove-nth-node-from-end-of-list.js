0a8d1a8fa31050f97de1e750e64ea84735186b9f
Shams Tabrez <shamsshoaib261@gmail.com>
2026-05-14 14:17:57 -0400

    Time: 0 ms (100%), Space: 55.3 MB (28.32%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
@@ -11,19 +11,33 @@
  * @return {ListNode}
  */
 var removeNthFromEnd = function(head, n) {
-    let sentinel = new ListNode()
-    sentinel.next = head
-    let first = sentinel
+
+    let dummy = new ListNode()
+    dummy.next = head
+    let slow= dummy
+    let fast = dummy
+
     for(let i=0;i<n;i++){
-        first = first.next
+        fast = fast.next
     }
-    let second = sentinel
-    while(first.next){
-       first = first.next
-        second = second.next
-        
+    while(fast.next){
+        slow = slow.next
+        fast = fast.next
     }
-    second.next= second.next.next
-    return sentinel.next
+    slow.next = slow.next.next
+    return dummy.next
     
 };
+
+//  4,6,9,3,8 > n =2
+
+// slow and fast > will start dummy
+
+
+// dummy>4>6>9>3>8
+// slow
+// fast >fast>fast
+// slow=4 , fast=9
+// slow=6, fast=3
+// slow=9,fast=8
+// slow.next = slow.next.next
